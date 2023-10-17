aa2cb752c7be7b37a831b660f17c17d242942e04
Indu <indu444333@gmail.com>
2023-10-17 21:35:04 +0530

    Day 26

---
+++ Day_26_20231017/JS_Basics/index.js
@@ -0,0 +1,263 @@
+// My first JS statement
+/*
+    This 
+    is inline
+*/
+console.log('Hello World')
+
+// Variables
+// -----------------
+var a = 'abc';
+console.log(a)
+let b = 'def'//Newer ways
+console.log(b)
+
+// Declaration
+let c;
+console.log(c)//undefined
+
+// Initialization
+let d = 'aaa'
+
+let firstName = 'John'
+let FirstName = 'John'
+// let 1test = '11'  ---
+// space '-' ---
+// _ is okay
+// Camelcase notation - eg: firstName
+
+// Constants
+// -------------------
+let aa = 'value 1'
+aa = 'value 2'
+
+const bb = 'Value 1'
+// bb = 'Value 2'  
+console.log(bb)
+
+// Primitive values
+// -----------------------------
+// -----------------------------
+// string, number, boolean, undefined, null, Symbol
+let name = 'John'
+console.log(name)
+let age = 30
+console.log(age)
+let isApproved = true; //false
+console.log(isApproved)
+let address;
+console.log(address)
+age = null
+console.log(age)
+
+// Dynamically typed - JS
+// Statically typed - C++, Java, ...
+
+// Referece Types
+// -----------------------------
+// -----------------------------
+// Object, array and function
+// 1)Object
+// -----------------------------
+let person = {
+    name: 'John',
+    age: 30
+}
+console.log(person)
+// 1) Dot notation
+console.log(person.name)
+console.log(person.age)
+person.age = 31;
+console.log(person)
+
+// 2) Bracket notation
+console.log(person['name'])
+console.log(person['age'])
+person['age'] = 32
+console.log(person)
+
+let propertyName = 'name'
+console.log(person[propertyName])
+
+// 2) Array
+// -----------------------------
+let arr = [ 10, 20, 30 ]
+arr = ['John', 30]
+console.log(arr)
+console.log(arr[0])
+console.log(arr[1])
+arr[1] = 31
+console.log(arr)
+arr[2] = 'Developer'
+console.log(arr)
+console.log(typeof(arr))
+console.log(arr.length)
+
+// 3) Function
+// -----------------------------
+//Declaration
+function greet(){
+    console.log('Hello World')
+}
+//Fn call
+greet();
+greet();
+greet();
+
+function greet2(name){
+    console.log('Hello ' + name)
+}
+greet2('John')
+
+function greet3(name, lastName){//Parameters
+    console.log('Hello ' + name + ' ' + lastName)
+}
+greet3('John','Smith')//Arguments
+
+// 2 type
+// 1 - Performing a task
+// 2 - Calculating a value
+
+function square(number){
+    return number * number;
+}
+
+let result = square(2);
+console.log(result)
+
+console.log(square(3));
+
+// Operators
+// -----------------------------
+// -----------------------------
+// Operators + some variables/constants -> Expresssions -> logic/algorithm
+// Operator, operand, operation
+
+// Arithmetic operators
+// -----------------------------
+let num1 = 3; //-> (7*2) + 1
+let num2 = 3;
+result = num1 + num2;
+console.log('num1 + num2 =', result)
+result = num1 - num2;
+console.log('num1 - num2 =', result)
+result = num1 * num2;
+console.log('num1 * num2 =', result)
+result = num1 / num2;
+console.log('num1 / num2 =', result)
+result = num1 % num2;
+console.log('num1 % num2 =', result)
+result = num1 ** num2;
+console.log('num1 ** num2 =', result)
+
+num1 = 1;
+num1++;//Increment -- Post-increment
+console.log('After post-incrementing:', num1)
+
+num1 = 1;
+++num1//Pre-increment
+console.log('After pre-incrementing:', num1)
+
+
+num2 = 2
+num2--//Decrement -- Post-decrement
+console.log('After post-decrementing:', num2)
+
+num2 = 2
+--num2//Pre-decrement
+console.log('After pre-decrementing:', num2)
+
+num1 = 1
+console.log(num1++)
+console.log(num1)
+num1 = 1
+console.log(++num1)
+console.log(num1)
+
+num2 = 2
+console.log(num2--)
+console.log(num2)
+num2 = 2
+console.log(--num2)
+console.log(num2)
+
+// Assignment operators
+// -----------------------------
+let x = 10;
+
+// x++
+// x = x + 1
+
+// x = x + 5
+x += 5
+
+// x += 10 ---> x = x + 10
+// x -= 10 ---> x = x - 10
+// x *= 10 ---> x = x * 10
+console.log(x)
+
+// Relational operators
+// -----------------------------
+num1 = 10;
+num2 = 20;
+
+console.log(num1 < num2); 
+console.log(num1 > num2);
+console.log(num1 == num2);
+console.log(num1 != num2);
+console.log(num1 === num2);
+console.log(num1 !== num2);
+console.log(num1 <= num2);
+console.log(num1 >= num2);
+
+// Equality
+// 1) ==  -- lose equality operator   -- value
+// 2) === -- strict equality operator -- value + type
+
+num1 = 10;
+num2 = '10';
+console.log(num1 == num2) //not checking the type
+console.log(num1 === num2)//checks the type also
+
+// Logical operators
+// -----------------------------
+// &&, ||, !   ---> AND, OR, NOT
+
+let a1 = 3
+let a2 = 2
+
+let b1 = 10
+let b2 = 20
+
+console.log((a1 < a2) && (b1 < b2))
+//  T && T -> T
+//  T && F -> F
+//  F && T -> F
+//  F && F -> F
+
+console.log((a1 < a2) || (b1 < b2))
+//  T || T -> T
+//  T || F -> T
+//  F || T -> T
+//  F || F -> F
+
+result = (a1 < a2) || (b1 < b2);
+console.log(result) //T
+//  ! T -> F
+//  ! F -> T
+console.log(!result) //F
+
+// Bitwise operators
+// 99 - Decimal number -> 0 to 9
+// binary - 0, 1
+// 1     - 0000 0001
+// 2     - 0000 0010
+// 1 & 2 - 0000 0000  -> 0
+console.log(1 & 2)//Bitwise AND operator
+
+// 1     - 0000 0001
+// 2     - 0000 0010
+// 1 | 2 - 0000 0011  -> 3
+console.log( 1 | 2)//Bitwise OR operator
+
+
