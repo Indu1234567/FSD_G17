27f8186edb911dd47de0d9d6d61b3388e0c66ed6
Indu <indu444333@gmail.com>
2023-11-09 20:59:55 +0530

    Day 39

---
+++ code/Day_39_20231109/JS_Basics/index.js
@@ -0,0 +1,9 @@
+const h2 = document.querySelector('h2')
+const ul = document.querySelector('ul')
+const items = document.querySelectorAll('li')
+
+// className
+h2.className = 'red'
+ul.className = 'red'
+ul.className = 'ul red'
+
