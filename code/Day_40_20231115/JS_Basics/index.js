236c27e01732b273557976c13909760775dc678a
Indu <indu444333@gmail.com>
2023-11-15 22:06:46 +0530

    Day_40_202311115

---
+++ code/Day_40_20231115/JS_Basics/index.js
@@ -0,0 +1,7 @@
+let img = document.querySelector('img');
+img.addEventListener('click', () => console.log('click'))
+img.addEventListener('dblclick', () => console.log('double click'))
+img.addEventListener('contextmenu', () => console.log('right click'))
+
+
+
